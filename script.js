041d8025207bf738cd7bc63ad11dcd13ce95f684
cutecactus-hub <themanedwolf999@gmail.com>
2025-07-15 20:03:05 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+ function countvowel(str) {
+
+    let count = 0;
+
+ for (const char of str) {
+    console.log(char);
+
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+        count++;
+    }
+
+ 
+ }
+}
